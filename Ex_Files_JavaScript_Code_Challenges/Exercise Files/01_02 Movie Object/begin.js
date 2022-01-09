cc4a6855dcfcd237ccc80f68a9245e61dc50357d
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-01-08 21:47:45 -0700

    Completed Movie Object Challenge

--- Ex_Files_JavaScript_Code_Challenges/Exercise Files/01_02 Movie Object/begin.js
+++ Ex_Files_JavaScript_Code_Challenges/Exercise Files/01_02 Movie Object/begin.js
@@ -1,3 +1,24 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+const Movie = (title, director, genre, releaseYear, rating) => {
+  return {
+    title,
+    director,
+    genre,
+    releaseYear,
+    rating,
+    getOverview() {
+      console.log(
+        `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+      );
+    }
+  }
+}
+
+const movieCollection = [];
+movieCollection.push(Movie('Star Wars: Episode IV - A New Hope', 'George Lucas', 'Action, Adventure, Fantasy', 1977, 8.6));
+movieCollection.push(Movie('Star Wars: Episode V - The Empire Strikes Back', 'George Lucas', 'Action, Adventure, Fantasy', 1977, 8.7));
+movieCollection.push(Movie('Star Wars: Episode VI - Return of the Jedi', 'George Lucas', 'Action, Adventure, Fantasy', 1977, 8.3));
+
+movieCollection.forEach(mov => mov.getOverview());
